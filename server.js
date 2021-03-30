2aeec35a5ef052435824e54531558d4b3fe050e2
Camilla Lähteenmäki <cakrla@utu.fi>
2021-03-30 17:26:13 +0300

    testi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world!</h1>");
+    res.send("<h1>MOI</h1>");
 });
 
 app.listen(8000, () => {
