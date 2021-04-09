1c1ccde653eb3e62fb5478aa5e577a2f8f629404
Sami Spets <savasp@utu.fi>
2021-04-09 22:56:39 +0300

    Added connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+require('dotenv').config();
 
 app.use(express.static(__dirname + "/build"));
 
-app.get("/", (req, res) => {
-    res.send("<h1>MOI</h1>");
-});
+mongoose.connect(
+    process.env.DBURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+}).then(() => console.log("Connected to database"))
+.catch(err => console.log(`Error while connecting to database ${err}`));
 
-app.listen(8000, () => {
-    console.log("piippiip");
+//app.post("/login")
+
+//app.post("/register")
+
+//app.post("/imageurl")
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`App running in port ${process.env.PORT}`);
 });
