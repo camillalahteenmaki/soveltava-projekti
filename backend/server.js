837b3207727d2d877ce7980a65ddcb96fc7bf323
Sami Spets <savasp@utu.fi>
2021-04-13 11:15:19 +0300

    Created POST /register route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const {createHash} = require("crypto")
 const app = express();
+const User = require('./user')
 require('dotenv').config();
 
+app.use(require("body-parser").urlencoded({ extended: false }));
+app.use(require("body-parser").json({limit: "50mb"}));
 app.use(express.static(__dirname + "/build"));
+const hash = createHash('sha256');
 
 mongoose.connect(
     process.env.DBURI, {
@@ -14,9 +19,23 @@ mongoose.connect(
 }).then(() => console.log("Connected to database"))
 .catch(err => console.log(`Error while connecting to database ${err}`));
 
+
 //app.post("/login")
 
-//app.post("/register")
+app.post("/register", async(req, res) => {
+    try{
+        const {username, password} = req.body;
+        if(!username || !password){
+            return res.status(400).json("User input failed");
+        }
+        const digest = hash.update(password).digest('hex');
+        const newUser = new User({username, password: digest});
+        await newUser.save();
+        return res.status(202).json("Success");
+    }catch(err){
+        return res.status(500).json("User already exists");
+    }
+})
 
 //app.post("/imageurl")
 
