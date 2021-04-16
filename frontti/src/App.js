1f67a451c1eec09901efd6fc8fb7b5af704b7137
Camilla Lähteenmäki <cakrla@utu.fi>
2021-04-16 12:43:07 +0300

    Kuvanlisäysformi, mut se ei toimi vielä kunnolla

--- frontti/src/App.js
+++ frontti/src/App.js
@@ -1,11 +1,17 @@
+import React, { Component } from "react";
 import './App.css';
+import ImageSearchForm from ".components/ImageSearchForm/ImageSearchForm";
+// import FaceDetect from "./components/FaceDetect/FaceDetect";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return  (
+      <div className "App">
+        <ImageSearchForm />
+        {/* <FaceDetect /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
